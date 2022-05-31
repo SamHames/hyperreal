b093e5c3d47ca220400a87a423f47dfc5a4124c2
Sam Hames <sam@hames.id.au>
2022-05-31 14:15:11 +1000

    A rough script to pull out the text of a post on a stack exchange site into a plaintext file

---
+++ test_data/stackoverflow_posts_to_text.py
@@ -0,0 +1,44 @@
+import csv
+from io import StringIO
+from html.parser import HTMLParser
+import sys
+import xml.etree.ElementTree as ET
+
+
+class MLStripper(HTMLParser):
+    """Via https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python"""
+
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+    xml_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    with open(output_file, "w") as outfile:
+        csvout = csv.writer(outfile, quoting=csv.QUOTE_ALL)
+        for row in root:
+            body = row.attrib["Body"]
+            body_text = strip_tags(body)
+            csvout.writerow([body_text])
